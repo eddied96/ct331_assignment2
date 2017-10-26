d14c47f70f49b34f870dd0c2c4abf9f77990a4a1
eddied96 <eddied96@users.noreply.github.com>
2017-10-26 23:45:03 +0100

    Assignment2
    ass2

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,1 +1,6 @@
 #lang racket
+(cons 1 2)
+(cons 1 (cons 2 (cons 3 null)))
+(cons 1 (cons 2 (cons 3 (cons "string" (cons 4 null)))))
+'(String 1 '(2 3 4))
+(append '(String) (append '(2) '(3 4 5)))
--- assignment_q2.rkt
+++ assignment_q2.rkt
@@ -1,17 +1,56 @@
 #lang racket
-
-;This is an example implementation of ins_beg,
-;It obviously doesn't do what it should, so you
-;can edit this function to get started.
-;
-;Please note the provide function is necessary
-;for the unit tests to work. Please include a
-;(provide) for each function you write in your
-;submitted assignment.
-;
-;You may delete these comments!
-
 (provide ins_beg)
+(provide ins_end)
+(provide cout_top_level)
+(provide count_instances)
+(provide count_instances_tr)
+(provide count_instances_deep)
+
 
 (define (ins_beg el lst)
-  (display "Hello, I'm ins_beg!\n"))
+  display "Hello, I'm ins_beg!\n"
+  (cons el lst))
+
+ 
+(define (ins_end el lst)
+  display "Hello, I'm ins_end!\n"
+  (append lst el))
+  
+(define (cout_top_level lst)
+  display "Counting elements in list...\n"
+  (length lst))
+
+(define (count_instances el lst)
+  (define element 0)
+  (display "Counting instances...\n")
+  (if(empty? lst) (error "No similar elements")(for([e (in-list lst)])(if(equal? e el)(set! element(+ element 1))(= element element))))
+  (display "The element " )
+  (display el)
+  (display " displays ")
+  (display element)
+  (display " times in the list"))
+(define (count_instances_tr el lst)
+(define (count_instances_tr_re el lst ctr)
+  (if (empty? lst)
+      (begin (display "Complete, list empty!\n") (display "Counting instances...\n")(display ctr))
+      (if(equal? (car lst) el)(count_instances_tr_re el (cdr lst) (+ ctr 1))(count_instances_tr_re el (cdr lst) ctr))))
+  (count_instances_tr_re el lst 0) 
+)
+(define (count_instances_deep el lst)
+  (define ctr 0)
+  (define (count_instances_deep_re lst)
+    (if (empty? lst)
+        (void)
+        (begin
+          (if (eq? el (car lst))
+                   (begin
+                     (set! ctr (+ ctr 1))
+                     (count_instances_deep_re (cdr lst)))
+                     (begin
+                       (if (number? (car lst))
+                              (count_instances_deep_re (cdr lst))
+                              (begin
+                                (count_instances_deep_re (car lst))
+                                     (count_instances_deep_re (cdr lst)))))))))
+  (count_instances_deep_re lst)
+  (display ctr))
